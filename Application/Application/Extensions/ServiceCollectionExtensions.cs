fd940f88102706a5e0aabbb8384886c7f3813847
nikita <368050@edu.itmo.ru>
2024-03-31 15:59:40 +0300

    fix mediatr di

--- Application/Application/Extensions/ServiceCollectionExtensions.cs
+++ Application/Application/Extensions/ServiceCollectionExtensions.cs
@@ -12,7 +12,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection collection)
     {
-        collection.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(IAssemblyMarker).Assembly));
+        collection.AddMediatR(typeof(IAssemblyMarker));
         collection.AddScoped<IValidator<CreateApplication.Command>, CreateApplicationValidator>();
         collection.AddScoped<IValidator<UpdateApplication.Command>, UpdateApplicationValidator>();
         collection.AddScoped<IValidator<SubmittedApplication>, SubmittedApplicationValidator>();
